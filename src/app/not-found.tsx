54e8dc2eb23c95b3534edf8dc9b3cbf8a7d7b3c9
mnaofficialbd <mna.official92@gmail.com>
2026-01-28 01:48:10 +0600

    404 page update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="text-center h-screen">
+      <h1 className="font-bold text-3xl text-red-600">4 0 4</h1>
+      <p className="text-md text-red-600">
+        Oops! The page you&apos;re looking for doesn’t exist.
+      </p>
+      <Link href="/">Go Back Home</Link>
     </div>
   );
 }
