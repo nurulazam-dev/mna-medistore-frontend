159fe4f6765d6fa9ab0afad456ea7dd111dd61cf
mnaofficialbd <mna.official92@gmail.com>
2026-02-02 04:55:32 +0600

    home page update

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,19 @@
 import Contact from "@/components/modules/homepage/Contact";
+import CustomerReview from "@/components/modules/homepage/CustomerReview";
+import Hero from "@/components/modules/homepage/Hero";
+import HowItWorks from "@/components/modules/homepage/HowItWorks";
+import KeyFeatures from "@/components/modules/homepage/KeyFeatures";
+import MedicineCategories from "@/components/modules/homepage/MedicineCategories";
 
 export default function Home() {
   return (
-    <div>
+    <main>
+      <Hero />
+      <KeyFeatures />
+      <MedicineCategories />
+      <HowItWorks />
+      <CustomerReview />
       <Contact />
-    </div>
+    </main>
   );
 }
